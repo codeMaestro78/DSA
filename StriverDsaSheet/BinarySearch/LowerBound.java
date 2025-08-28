d5b531d4322aa2fb821fef5d5da5a8934919f971
codeMaestro78 <thelogical369@gmail.com>
2025-08-29 01:30:21 +0530

    add binary search

---
+++ StriverDsaSheet/BinarySearch/LowerBound.java
@@ -0,0 +1,40 @@
+package StriverDsaSheet.BinarySearch;
+
+public class LowerBound {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int x = 7;
+        System.out.println((lowerBound(arr, arr.length, x)));
+        System.out.println((upperbound(arr, arr.length, x)));
+    }
+
+    static int lowerBound(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static int upperbound(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] > x) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
