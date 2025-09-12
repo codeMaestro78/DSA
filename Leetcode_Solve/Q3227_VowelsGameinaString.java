913836bbde388003acc5da56376541d3fcf72cee
LALANI DEVARSHI <thelogical369@gmail.com>
2025-09-12 16:08:21 +0530

    daily leetcode (streak 4)

--- Leetcode_Solve/Q3227_VowelsGameinaString.java
+++ Leetcode_Solve/Q3227_VowelsGameinaString.java
@@ -1,4 +1,20 @@
 package Leetcode_Solve;
 
 public class Q3227_VowelsGameinaString {
+    public static void main(String[] args) {
+        System.out.println(doesAliceWin("bbdd"));
+    }
+
+    static boolean doesAliceWin(String s) {
+        int vowels = 0;
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) vowels++;
+        }
+        return vowels % 2 == 1;
+    }
+
+    static boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
+    }
+
 }
