0a543affbc46f152cab2a7648c46eb274481d908
LALANI DEVARSHI <thelogical369@gmail.com>
2025-09-23 21:28:05 +0530

    add leetcode and binary tree

---
+++ DataStructure/Trees/BinaryTree.java
@@ -0,0 +1,81 @@
+package DataStructure.Trees;
+
+import java.util.*;
+
+public class BinaryTree {
+
+    public BinaryTree() {
+
+    }
+
+    static class Node {
+        private int val;
+        private Node left;
+        private Node right;
+
+        public Node(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    private Node root;
+
+    // insert elements
+    public void populate(Scanner scanner) {
+        System.out.println("Enter the root node:- ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner, root);
+    }
+
+    public void populate(Scanner sc, Node node) {
+        System.out.println("Do you want to enter left of " + node.val);
+        boolean left = sc.nextBoolean();
+        if (left) {
+            System.out.println("enter the value of the left of " + node.val);
+            int value = sc.nextInt();
+            node.left = new Node(value);
+            populate(sc, node.left);
+        }
+        System.out.println("Do you want to enter right of " + node.val);
+        boolean right = sc.nextBoolean();
+        if (right) {
+            System.out.println("enter the value of the right of " + node.val);
+            int value = sc.nextInt();
+            node.right = new Node(value);
+            populate(sc, node.right);
+        }
+    }
+
+    public void prettyDisplay() {
+        prettyDisplay(root, 0);
+    }
+
+    public void prettyDisplay(Node node, int level) {
+        if (node == null) {
+            return;
+        }
+        prettyDisplay(node.right, level + 1);
+
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
+            }
+            System.out.println("|--------->" + node.val);
+
+        } else {
+            System.out.println(node.val);
+        }
+        prettyDisplay(node.left, level + 1);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinaryTree bt = new BinaryTree();
+        bt.populate(sc);
+        bt.prettyDisplay();
+    }
+}
--- Leetcode_Solve/Q3005_CountElementsWithMaximumFrequency.java
+++ Leetcode_Solve/Q3005_CountElementsWithMaximumFrequency.java
@@ -1,4 +1,30 @@
 package Leetcode_Solve;
 
+import java.awt.print.PrinterGraphics;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q3005_CountElementsWithMaximumFrequency {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 4, 5, 6, 7, 8, 8, 9};
+        System.out.println(maxFrequencyElements(arr));
+    }
+    static int maxFrequencyElements(int[] nums) {
+        int freq = 0;
+        int result = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int count : map.values()) {
+            freq = Math.max(freq, count);
+        }
+        for (int count : map.values()) {
+            if (count == freq) {
+                result += count;
+            }
+        }
+        return result;
+    }
 }
