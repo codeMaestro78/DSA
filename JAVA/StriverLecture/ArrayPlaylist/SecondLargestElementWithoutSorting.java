df3069a9e20720f3e4fb13a3db39d36dc25ae008
codeMaestro78 <thelogical369@gmail.com>
2026-06-06 07:21:54 +0000

    Add code

---
+++ JAVA/StriverLecture/ArrayPlaylist/SecondLargestElementWithoutSorting.java
@@ -0,0 +1,23 @@
+package StriverLecture.ArrayPlaylist;
+
+public class SecondLargestElementWithoutSorting {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+
+        System.out.println(secondLargest(arr));
+    }
+
+    public static int secondLargest(int[] arr) {
+        int MAX = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > MAX) {
+                secondMax = MAX;
+                MAX = arr[i];
+            } else if (arr[i] > secondMax && arr[i] != MAX) {
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+    }
+}
